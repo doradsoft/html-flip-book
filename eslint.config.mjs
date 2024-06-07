25d99c61861a0353f6668770740d43065ff332aa
ruben hagege <hagege.ruben@gmail.com>
2024-06-07 14:44:30 +0300

    fix mosty of the lint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,41 @@
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
-
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginJsonc from "eslint-plugin-jsonc";
 
 export default [
-  { ignores: ["**/dist/*"] },
-  { languageOptions: { globals: globals.browser } },
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  ...fixupConfigRules(pluginReactConfig)
+  {
+    ignores: ["**/dist/*", "**/lib/*", "**/node_modules/*", "**/coverage/*"]
+  },
+  {
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      globals: globals.browser
+    }
+  },
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  fixupConfigRules(pluginReactConfig),
+  {
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "prettier": eslintPluginPrettier,
+      "jsonc": eslintPluginJsonc
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:jsonc/recommended-with-jsonc",
+      "plugin:prettier/recommended"
+    ],
+    rules: {
+      "prettier/prettier": "error",
+      "jsonc/array-bracket-spacing": ["error", "never"],
+      "jsonc/indent": ["error", 2],
+      "jsonc/object-curly-spacing": ["error", "always"],
+      "@typescript-eslint/no-unused-vars": ["error"]
+    }
+  }
 ];
