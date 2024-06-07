25457575f210478ecc4353a09a2b2d110eefa8bd
ruben hagege <hagege.ruben@gmail.com>
2024-06-07 13:28:29 +0300

    maybe better?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
 
 export default [
   { ignores: ["**/dist/*"] },
   { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+  ...fixupConfigRules(pluginReactConfig)
 ];
