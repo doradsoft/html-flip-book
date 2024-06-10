63347d3e8630fd7ba43146f545dcba2f87be6a2f
ruben hagege <hagege.ruben@gmail.com>
2024-06-11 01:36:11 +0300

    Updates

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
-export default [
-  { ignores: ["**/dist/*"] },
-  { languageOptions: { globals: globals.browser } },
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-];
